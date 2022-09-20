dd3de54aa83ea7fbbefa03d445dcc33b03b67c5f
SaadOcloud <saad.fareed@ocloudsolutions.net>
2022-09-20 17:30:31 +0500

    Task 1 correct approach

--- Task1.js
+++ Task1.js
@@ -1,17 +1,18 @@
-function groupcity(array,key){
-    return array.filter(a=>{
-        return a.city==key
-    })
+function groupByKey(array, key) {
+  return array.reduce((hash, obj) => {
+      if(obj[key] === undefined) return hash; 
+      return Object.assign(hash, { [obj[key]]:( hash[obj[key]] || [] ).concat(obj)})
+    }, {})
 }
 
 const array= [
-    {id:1,name:"Bilal", city:"Lahore"},
-    {id:1,name:"Bilal", city:"Lahore"},
-    {id:3,name:"Hafsa", city:"Karachi"},
-    {id:4,name:"Rehan", city:"Lahore"},
-    {id:5,name:"Saqib", city:"Karachi"},
-    {id:6,name:"Farhan", city:"Islamabad"}
-]
-console.log(groupcity(array,"Karachi"))
-console.log(groupcity(array,"Lahore"))
-console.log(groupcity(array,"Islamabad"))
+  {id:1,name:"Bilal", city:"Lahore"},
+  {id:1,name:"Bilal", city:"Lahore"},
+  {id:3,name:"Hafsa", city:"Karachi"},
+  {id:4,name:"Rehan", city:"Lahore"},
+  {id:5,name:"Saqib", city:"Karachi"},
+  {id:6,name:"Farhan", city:"Islamabad"}
+  ];
+
+var result = groupByKey(array, 'city');
+console.log(result)
