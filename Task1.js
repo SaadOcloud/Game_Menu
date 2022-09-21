4c1a6800bcf61b014bc995a920f1505d1973e3d4
SaadOcloud <saad.fareed@ocloudsolutions.net>
2022-09-21 12:59:25 +0500

    Conflicts Resolve

--- Task1.js
+++ Task1.js
@@ -1,18 +1,27 @@
-function groupByKey(array, key) {
-  return array.reduce((hash, obj) => {
-      if(obj[key] === undefined) return hash; 
-      return Object.assign(hash, { [obj[key]]:( hash[obj[key]] || [] ).concat(obj)})
-    }, {})
+function GroupByKey(array,key)
+{
+  var tempData = [];
+
+  for ( var index=0; index<array.length; index++ )
+   {
+      if ( !tempData[array[index][key]]) 
+      {
+          tempData[array[index][key]]=[];
+          
+      }
+      tempData[array[index][key]].push(array[index] );
+  }
+  return tempData
 }
-
 const array= [
-  {id:1,name:"Bilal", city:"Lahore"},
-  {id:1,name:"Bilal", city:"Lahore"},
-  {id:3,name:"Hafsa", city:"Karachi"},
-  {id:4,name:"Rehan", city:"Lahore"},
-  {id:5,name:"Saqib", city:"Karachi"},
-  {id:6,name:"Farhan", city:"Islamabad"}
-  ];
+    {id:1,name:"Bilal", city:"Lahore"},
+    {id:1,name:"Bilal", city:"Lahore"},
+    {id:3,name:"Hafsa", city:"Karachi"},
+    {id:4,name:"Rehan", city:"Lahore"},
+    {id:5,name:"Saqib", city:"Karachi"},
+    {id:6,name:"Farhan", city:"Islamabad"}
+    ];
 
-var result = groupByKey(array, 'city');
-console.log(result)
+let key='city';
+
+GroupByKey(array,key)
